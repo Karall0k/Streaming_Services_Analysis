82743201217548327a0ef2658e85847fcf0024b9
Karesse Lockard <karalockard@Karesses-MacBook-Pro.local>
2024-06-10 23:16:37 -0400

    updated schema

--- Resources/Schema_1.sql
+++ Resources/Schema_1.sql
@@ -1,6 +1,9 @@
 -- Drop table if exists--
 DROP TABLE "Combined Streaming Dataset";
+-- Drop table if exists--
+DROP TABLE "Ratings Summary";
 
+--Create main table--
 Create Table "Combined Streaming Dataset" (
  	show_id VARCHAR(10) NOT NULL,
 	type VARCHAR(10),
@@ -14,5 +17,19 @@ Create Table "Combined Streaming Dataset" (
 	streaming_service VARCHAR(10) NOT NULL,
 	PRIMARY KEY (show_id,streaming_service)
 );
-
+--view table--
 Select * FROM "Combined Streaming Dataset";
+
+--Create summary table--
+Create Table "Ratings Summary" (
+ 	" " VARCHAR(10) NOT NULL,
+	Netflix dec NOT NULL,
+	Hulu dec NOT NULL,
+	Amazon dec NOT NULL,
+	Disney dec NOT NULL
+);
+-- Import Ratings_Summary.csv data before running this code--
+Alter Table "Ratings Summary"
+RENAME COLUMN " " to "Rating Category";
+
+Select * FROM "Ratings Summary"
